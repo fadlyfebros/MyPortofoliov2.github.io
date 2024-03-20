b9c523905d02a1057e3f561ccb7d6cd85adc0e89
Fadly Febro <fadlyfebro@gmail.com>
2024-03-21 06:22:23 +0700

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Halo, Saya Fadly Febro</span> Saya seorang mahasiswa aktif Teknik Informatika Universitas YARSI. Saya Mengetahui tentang Pengembang Web. Selama kuliah, saya senang bekerja dalam tim, komunikasi yang baik, dan inisiatif yang tinggi. Saat ini saya sedang dalam pencarian untuk mengembangkan kepribadian dan keterampilan saya untuk mencapai tujuan saya di masa depan"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
